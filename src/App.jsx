cadceb94026b82c5486d95b51bd2d775824ebda0
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-05 13:16:02 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import QRCode from 'qrcode';
+import { useState } from 'react';
+
+const App = () => {
+    const [url, setUrl] = useState('');
+    const [qrcode, setQrcode] = useState('');
+
+    const GenerateQRCode = () => {
+        QRCode.toDataURL(
+            url,
+            { margin: 2, color: { dark: '#0000ffff' } },
+            (err, url) => {
+                if (err) return console.error(err);
+                setQrcode(url);
+            }
+        );
+    };
+
+    return (
+        <div className='app'>
+            <h1>QR Code Generator</h1>
+            <input
+                type='text'
+                placeholder='Enter https://'
+                value={url}
+                onChange={(event) => setUrl(event.target.value)}
+            />
+            <button onClick={GenerateQRCode}>Generate</button>
+            {qrcode && (
+                <>
+                    <img src={qrcode} alt='photo_qrcode' />
+                    <a href={qrcode} download='qrcode.png'>
+                        Download QR Code
+                    </a>
+                </>
+            )}
+        </div>
+    );
+};
+export default App;
